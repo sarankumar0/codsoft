88bed5347cc541b4c451463e55b30293d0f51e15
sarankumar0 <122343606+sarankumar0@users.noreply.github.com>
2024-08-08 23:16:17 +0530

    Add files via upload

---
+++ BankAccount-atm.java
@@ -0,0 +1,27 @@
+package Codsoft;
+
+public class BankAccount {
+        private double balance;
+
+        public BankAccount(double initialBalance) {
+            this.balance = initialBalance;
+        }
+
+        public double getBalance() {
+            return balance;
+        }
+
+        public boolean withdraw(double amount) {
+            if (amount > balance) {
+                return false; // Insufficient balance
+            } else {
+                balance -= amount;
+                return true;
+            }
+        }
+
+        public void deposit(double amount) {
+            balance += amount;
+        }
+    }
+
